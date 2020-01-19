d7af0711696de84a3be5bd07ec73cccf596b10fa
Marian Bolojan <office.adorade@gmail.com>
2020-01-19 17:45:38 +0200

    build(tools): add mozjpeg plugin

--- tools/tasks/images.js
+++ tools/tasks/images.js
@@ -4,7 +4,7 @@
  * Licensed under MIT
  * ========================================================================== */
 
-import { src, dest, lastRun, $, bs, magenta, green, paths, opts } from '../util';
+import { src, dest, lastRun, $, bs, green, magenta, paths, opts } from '../util';
 
 // For debugging usage:
 // .pipe($.debug({ title: 'unicorn:' }))
@@ -27,6 +27,7 @@ export function imagine () {
       $.imagemin.optipng(opts.images.png),
       $.imagemin.svgo(opts.images.svg)
     ], { verbose: true }))
+    // .pipe($.size(opts.size))
     .pipe(dest(paths.images.dest))
     .pipe(bs.stream({ match: '**/*.{gif,jpg,jpeg,png,svg}' }));
 }
